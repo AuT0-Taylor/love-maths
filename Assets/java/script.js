e581d544ea1c5406b3a2d6b777c2288cbd6c244c
AuT0-Taylor <147276635+AuT0-Taylor@users.noreply.github.com>
2024-08-04 20:58:24 +0000

    script structure

--- Assets/java/script.js
+++ Assets/java/script.js
@@ -1,1 +1,35 @@
 console.log("connecteed")
+
+function runGame(){
+
+}
+
+
+function checkAnswer(){
+
+}
+
+
+function calculateCorrectAnswer(){
+
+}
+
+
+function incrementScore(){
+
+}
+
+
+function incrementWrongScore(){
+
+}
+
+
+function displayAdditionQuestion(){
+
+}
+
+
+function displayMultiplyQuestion(){
+    
+}
