10f7bd0fdf1e51336c9f2046f47dbd023cff4da3
AuT0-Taylor <147276635+AuT0-Taylor@users.noreply.github.com>
2024-08-04 21:11:00 +0000

    js sript event listners test

--- Assets/java/script.js
+++ Assets/java/script.js
@@ -1,4 +1,17 @@
-console.log("connecteed")
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of button){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "sumbit"){
+                alert("you clicked sumbit");
+            } else {
+                let gameType =  this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`)
+            }
+        })
+    }
+})
 
 function runGame(){
 
@@ -6,30 +19,24 @@ function runGame(){
 
 
 function checkAnswer(){
-
 }
 
 
 function calculateCorrectAnswer(){
-
 }
 
 
 function incrementScore(){
-
 }
 
 
 function incrementWrongScore(){
-
 }
 
 
 function displayAdditionQuestion(){
-
 }
 
 
 function displayMultiplyQuestion(){
-    
 }
