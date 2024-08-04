5b8da982179c837339668ce61357f4f4e69fdf0d
AuT0-Taylor <147276635+AuT0-Taylor@users.noreply.github.com>
2024-08-04 22:44:41 +0000

    added script to check user answers

--- Assets/java/script.js
+++ Assets/java/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -34,12 +34,41 @@ function runGame(gameType){
 
 }
 
-
+/**
+ * checks answers from user and calculatorcorrectanswer()
+ */
 function checkAnswer(){
+
+    let userAnswer =  parseInt(document.getElementById("answer-box").value);
+    let calculateAnswer =  calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculateAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey You got it!");
+    } else {
+        alert(`wrong sorry ${userAnswer}. the right answer was ${calculateAnswer[0]}!`);
+    }
+
+    runGame(calculateAnswer[1]);
 }
 
 
+
+/**
+ * calculates the operands
+ */
 function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}.Aborting!`
+    }
+
 }
 
 
